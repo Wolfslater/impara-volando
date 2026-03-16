cf513a2abb17f85661db61efc0b7072475bc10bd
wolfslater <ddisante88@gmail.com>
2026-03-16 15:59:02 +0100

    aggiunta input da tastiera per direzioni up/down e rotazione oraria/antioraria

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 from djitellopy import tello
-from time import sleep
+from sys import exit
 from keyboard import is_pressed
 import cv2
 
@@ -7,21 +7,26 @@ drone = tello.Tello()
 drone.connect()
 print(f"{drone.get_battery()}")
 
-move = 20
+speed = 50
 control = [0,0,0,0]
 
 drone.streamon()
 while True:
+    control = [0,0,0,0]
+    if is_pressed("c"): exit()
     if is_pressed("z") and not drone.is_flying: drone.takeoff()
     if is_pressed("x") and drone.is_flying: drone.land()
-    if is_pressed("w"): control[1] += 1 
-    if is_pressed("s"): control[1] -= 1
-    if is_pressed("a"): control[0] -= 1
-    if is_pressed("d"): control[0] += 1
-    elif is_pressed(" "): control = [0,0,0,0]
+    if is_pressed("w"): control[1] = speed
+    if is_pressed("s"): control[1] = -speed
+    if is_pressed("a"): control[0] = -speed
+    if is_pressed("d"): control[0] = speed
+    if is_pressed("j"): control[3] = speed
+    if is_pressed("l"): control[3] = -speed
+    if is_pressed("i"): control[2] = speed
+    if is_pressed("k"): control[2] = -speed
+    if is_pressed(" "): control = [0,0,0,0]
 
     #print(*control)
-    control = [0,0,0,0]
     drone.send_rc_control(*control)
     img = drone.get_frame_read().frame
     cv2.imshow("prova", img)
