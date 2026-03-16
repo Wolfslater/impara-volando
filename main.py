09b4250f78a14ebfe3d893575ccaab58c623f251
wolfslater <ddisante88@gmail.com>
2026-03-16 18:00:59 +0100

    correzione di alcune falle logiche a mente fredda

--- main.py
+++ main.py
@@ -4,12 +4,14 @@ from keyboard import is_pressed
 import cv2
 
 def draw_speed(img, x, y, speed): #creats a rectangle that shows the drone speed
-    if is_pressed("q") and speed < 105: speed += 5
-    if is_pressed("e") and speed > -100: speed -= 5
+    if is_pressed("q") and speed < 100: speed += 5
+    if is_pressed("e") and speed > 0: speed -= 5
     cv2.rectangle(img, (x, y), (x + 30, y + 100), (255, 255, 0), 3)
     cv2.rectangle(img, (x, y + 100 - speed), (x + 30, y + 100), (25, 255, 0), cv2.FILLED)
+    return speed
 
 def send_control(control):
+    global speed
     if is_pressed("c"): exit()
     if is_pressed("z") and not drone.is_flying: drone.takeoff()
     if is_pressed("x") and drone.is_flying: drone.land()
@@ -21,7 +23,7 @@ def send_control(control):
     if is_pressed("l"): control[3] = speed
     if is_pressed("i"): control[2] = speed
     if is_pressed("k"): control[2] = -speed
-    if is_pressed(" "): control = [0,0,0,0]
+    if is_pressed("-"): control = [0,0,0,0]
     return control
 
 if __name__ == '__main__':
@@ -36,7 +38,7 @@ if __name__ == '__main__':
     while True:
         oldcontrol = control.copy()
         control = [0,0,0,0]
-        send_control(control)
+        control = send_control(control)
 
         #print(*control)
         if (control!=oldcontrol):
@@ -45,6 +47,6 @@ if __name__ == '__main__':
         img = cv2.cvtColor(img, cv2.COLOR_RGB2BGR)
         cv2.putText(img, f"Battery life: {drone.get_battery()}%",
             (690, 100), cv2.QT_FONT_NORMAL, 1, (50, 240, 255), 2)
-        draw_speed(img, 10, 10, speed)
+        speed = draw_speed(img, 10, 10, speed)
         cv2.imshow("prova", img)
         cv2.waitKey(1)
