8ff60ff1879a8c8112570a0fe37f84fccd9fd3a4
wolfslater <ddisante88@gmail.com>
2026-05-11 16:09:39 +0200

    integrazione hand detection

--- main_diego.py
+++ main_diego.py
@@ -1,7 +1,11 @@
 from djitellopy import tello
 from sys import exit
-from keyboard import is_pressed
 import cv2
+from cvzone.HandTrackingModule import HandDetector
+detector = HandDetector(maxHands=2)
+from keyboard import is_pressed
+
+key = 0
 
 def draw_speed(img, x, y, speed): #creats a rectangle that shows the drone speed
     if is_pressed("q") and speed < 100: speed += 1
@@ -32,19 +36,31 @@ if __name__ == '__main__':
     print(f"{drone.get_battery()}")
 
     speed = 50
+    key = 0
     control = [0,0,0,0]
 
     drone.streamon()
-    while True:
+    while key!=27:
         oldcontrol = control.copy()
         control = [0,0,0,0]
         control = send_control(control)
 
-        print(*control)
+        #print(*control)
         if (control!=oldcontrol):
             drone.send_rc_control(control[0], control[1], control[2], control[3])
         img = drone.get_frame_read().frame
         img = cv2.cvtColor(img, cv2.COLOR_RGB2BGR)
+        hands, img = detector.findHands(img, draw=True)
+        if hands:
+            hand = hands[0]
+            x,y,w,h = hand['bbox']
+            type = hand['type']
+            center = hand['center']
+            lmList = hand['lmList']
+            fingers = detector.fingersUp(hand)
+            if fingers[0] == 1: print("debug", fingers)
+            cv2.rectangle(img, (x - 20, y - 20), (x + w + 20, y + h + 20), (0, 255, 0), 2)
+            cv2.putText(img, type, (x - 20, y - 30), cv2.FONT_HERSHEY_PLAIN, 1, (0, 255, 0), 2)
         cv2.putText(img, f"Battery life: {drone.get_battery()}%",
             (690, 100), cv2.QT_FONT_NORMAL, 1, (50, 240, 255), 2)
         speed = draw_speed(img, 10, 10, speed)
